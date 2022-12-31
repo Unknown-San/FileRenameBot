a1655765152b6851c065fce03e9d5d928f495cef
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-12-31 17:31:07 +0530

    Update video_converter.py

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -12,8 +12,7 @@ import time
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
-else:
-    from config import Config
+
 
 # the Strings used for this "thing"
 from translation import Translation
