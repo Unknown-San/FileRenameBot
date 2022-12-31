223836f796049bd08ba6258e94848851753457d9
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-12-31 17:40:45 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -11,7 +11,7 @@ import time
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
+    from config import Config
 
 
 # the Strings used for this "thing"
